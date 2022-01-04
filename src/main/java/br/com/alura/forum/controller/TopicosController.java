cadbc7b249587dbf674895f06b76e310daccae5f
wtrindades <wtrindades@gmail.com>
2022-01-04 12:01:32 -0300

    Refactor TopicosController#list to use Pageble of SpringDataWebSupport

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,16 +1,13 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -43,13 +40,7 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
-	public Page<TopicoDto> lista(
-			@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, 
-			@RequestParam int qtd, @RequestParam String ordenacao
-			) {
-		
-		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.ASC, ordenacao);
-		
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, Pageable paginacao) {
 		Page<Topico> topicos;
 		
 		if (nomeCurso == null) {
