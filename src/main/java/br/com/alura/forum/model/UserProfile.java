c2616e3f890c9b85db4709b3a089267b1d33dd49
wtrindades <wtrindades@gmail.com>
2022-01-06 09:15:07 -0300

    feat: add security auth in User

---
+++ src/main/java/br/com/alura/forum/model/UserProfile.java
@@ -0,0 +1,35 @@
+package br.com.alura.forum.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+public class UserProfile implements GrantedAuthority {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String getAuthority() {
+		return name;
+	}
+}
