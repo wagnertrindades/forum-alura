ece3026899044bda518cd0877b5efef01d3316c8
wtrindades <wtrindades@gmail.com>
2022-01-10 19:06:13 -0300

    feat: change config to SpringFox 3.0 and removes actuator because incompatibility

---
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
@@ -0,0 +1,34 @@
+package br.com.alura.forum.config.swagger;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.alura.forum.model.User;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfigurations {
+
+	@Bean
+	public Docket forumApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
+				.paths(PathSelectors.ant("/**"))
+				.build()
+				.ignoredParameterTypes(User.class)
+				.globalRequestParameters(Arrays.asList(
+						new RequestParameterBuilder()
+						.name("Authorization")
+						.description("Header para Token JWT")
+						.required(false)
+						.in("header")
+						.build()));
+	}
+}
