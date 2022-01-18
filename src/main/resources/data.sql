b38c9fd84b681f3ff91a6787a408c998a52e8ce9
wtrindades <wtrindades@gmail.com>
2022-01-18 10:55:08 -0300

    chore: add user profile moderator data in DB

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,11 @@
 INSERT INTO USER(name, email, password) VALUES('Aluno', 'aluno@email.com', '$2a$10$iB5HhSFEykYSWahVr2pa6O1IoCduiSNGxYOkLu8T8bncNeBmXgKHi');
+INSERT INTO USER(name, email, password) VALUES('Moderador', 'moderador@email.com', '$2a$10$iB5HhSFEykYSWahVr2pa6O1IoCduiSNGxYOkLu8T8bncNeBmXgKHi');
+
+INSERT INTO USER_PROFILE(id, name) VALUES(1, 'ROLE_STUDENT');
+INSERT INTO USER_PROFILE(id, name) VALUES(2, 'ROLE_MODERATOR');
+
+INSERT INTO USER_PROFILES(user_id, profiles_id) VALUES(1, 1);
+INSERT INTO USER_PROFILES(user_id, profiles_id) VALUES(2, 2);
 
 INSERT INTO COURSE(name, category) VALUES('Spring Boot', 'Programação');
 INSERT INTO COURSE(name, category) VALUES('HTML 5', 'Front-end');
